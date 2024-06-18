270440fbad734fca439900b43f10bc165bd5e7a4
HaJunRyu <hjr4557@naver.com>
2024-06-18 16:18:55 +0900

    Feat: csr, ssr로 페이지로 이동 가능한 Link를 추가한다

--- src/app/csr/page.tsx
+++ src/app/csr/page.tsx
@@ -1,10 +1,17 @@
 import { fetchPosts } from '@/api/posts';
+import Link from 'next/link';
 
 export default async function CsrPage() {
   const posts = await fetchPosts({ isSsr: false });
 
   return (
     <div>
+      <Link href="/ssr">
+        <h2>
+          SSR <span>-&gt;</span>
+        </h2>
+      </Link>
+
       {posts.map(post => (
         <div key={post.title}>{post.title}</div>
       ))}
--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -1,10 +1,17 @@
 import { fetchPosts } from '@/api/posts';
+import Link from 'next/link';
 
 export default async function SsrPage() {
   const posts = await fetchPosts({ isSsr: true });
 
   return (
     <div>
+      <Link href="/csr">
+        <h2>
+          CSR <span>-&gt;</span>
+        </h2>
+      </Link>
+
       {posts.map(post => (
         <div key={post.title}>{post.title}</div>
       ))}
